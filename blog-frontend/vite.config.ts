37d637186138120a5f4c641a2b111571d5a1fae6
n10r <naveen.1.r@nokai.com>
2026-04-14 12:35:34 +0530

    Deployment changes (3)

--- blog-frontend/vite.config.ts
+++ blog-frontend/vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          editor: ['@tiptap/react', '@tiptap/starter-kit'],
+          ui: ['@nextui-org/react']
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
@@ -13,4 +24,3 @@ export default defineConfig({
     }
   }
 })
-
